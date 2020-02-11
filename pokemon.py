4ce8b10d2d310259d518898e295e4f5f8ef06410
lukesnc <lukeasimone@gmail.com>
2020-02-11 12:05:40 -0500

    removed bulbasaur

--- pokemon.py
+++ pokemon.py
@@ -3,46 +3,50 @@
 
 # Imports
 from type import Type
+from random import choice
 
-class Pokemon:
+class Pokemon(object):
     # Attributes
-    name = ""
-    nickname = ""
+    name, nickname = ""
+    pokedexNum = None
     type = '???'
     gender = None
-    moves = []
-    nature = None
-    pokedexNum = None
+    nature = random.choice(['Adamant','Bashful','Bold',
+                            'Brave','Calm','Careful',
+                            'Docile','Gentle','Hardy','Hasty',
+                            'Impish','Jolly','Lax','Lonely',
+                            'Mild','Modest','Naive','Naughty',
+                            'Quiet','Quirky','Rash','Relaxed',
+                            'Sassy','Serious','Timid'])
+    ball = None
 
-    # Stats
-    hp = None
-    attack = None
-    defense = None
-    spAtk = None
-    spDef = None
-    speed = None
+    # Base stats
+    bHp, bAtk, bDef, bSpAtk, bSpDef, bSpd = None
 
-    # Leveling
+    # Current stats
+    hp, atk, def, spAtk, spDef, spd = None
     level = 1
     exp = 0
     evolution = 0
+    moves = []
 
     # Battle status (asleep, confused, etc.)
     status = None
     heldItem = None
 
-    def __init__(self, nickname=""):
-        self.nickname = nickname
-
+    def __init__(self, nn="", ball="Poke Ball", lvl=1):
+        self.nickname = nn
+        self.ball = ball
+        self.level = lvl
 
     def show_stats(self):
         print("Name:", self.name)
         print("Nickname:", self.nickname)
+        print("Pokedex #:", self.pokedexNum)
         print("Type(s):", self.type)
         print("Gender:", self.gender)
-        print("Moves:", self.moves)
         print("Nature:", self.nature)
-        print("Pokedex #:", self.pokedexNum)
+        print("Ball:", self.ball)
         print("HP:", self.hp)
         print("Attack:", self.attack)
         print("Defense:", self.defense)
@@ -51,6 +55,7 @@ class Pokemon:
         print("Speed:", self.speed)
         print("Level:", self.level)
         print("Exp. Points:", self.exp)
+        print("Moves:", self.moves)
 
     def fight(self):
         pass
